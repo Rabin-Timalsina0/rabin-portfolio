6aca9281c5391828cdb651488cfc8b4a371ebf83
Rabin Timalsina <rabintimalsina@Rabins-MacBook-Air.local>
2025-07-01 19:42:05 +0545

    Updated something

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -77,7 +77,7 @@ const Navbar = () => {
             fontSize="2xl"
             fontWeight="bold"
           >
-            Rabin Timalsina
+            Rabin Gracias
           </Text>
         </Flex>
 
